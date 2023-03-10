ac0d680df9666b647efcc84191bda2255365df23
Learn Today <learntodayfromai@gmail.com>
2023-03-10 14:36:59 -0800

    initial commit

---
+++ app/src/main/java/com/learntodayfromai/aiexplorer/ui/home/HomeFragment.kt
@@ -0,0 +1,63 @@
+package com.learntodayfromai.aiexplorer.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.support.v4.app.Fragment
+import android.arch.lifecycle.ViewModelProvider
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.learntodayfromai.aiexplorer.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(
+                this,
+                ViewModelProvider.NewInstanceFactory()
+            ).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val webView: WebView = binding.webview
+        webView.settings.javaScriptEnabled = true;
+        webView.clearCache(true)
+        webView.loadUrl("https://www.google.com")
+        // TODO this syntax is kind of weird, look at it later
+        webView.webViewClient = object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                url: String
+            ): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+
+        //what is it? TODO lets look at this in a future video
+        /*homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }*/
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

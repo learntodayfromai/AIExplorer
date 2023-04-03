bc04449c8a90d333acb36f64c0ed476c96d2d796
Learn Today <learntodayfromai@gmail.com>
2023-04-03 11:31:49 -0700

    remove deprecated code

--- app/src/main/java/com/learntodayfromai/aiexplorer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/learntodayfromai/aiexplorer/ui/home/HomeFragment.kt
@@ -25,12 +25,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(
-                this,
-                ViewModelProvider.NewInstanceFactory()
-            ).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
@@ -38,7 +32,7 @@ class HomeFragment : Fragment() {
         webView.settings.javaScriptEnabled = true;
         webView.clearCache(true)
         webView.loadUrl("https://www.google.com")
-        // TODO this syntax is kind of weird, look at it later
+
         webView.webViewClient = object : WebViewClient(){
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
@@ -48,11 +42,6 @@ class HomeFragment : Fragment() {
                 return true
             }
         }
-
-        //what is it? TODO lets look at this in a future video
-        /*homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }*/
         return root
     }
 
